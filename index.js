b5ef0bed87b5dd1610cbce86442863e6d5e8e659
piyushraj2340 <piyushraj2340@gmail.com>
2020-06-02 21:40:12 +0530

    <!--Web Dev-->
    02-06-2020 UpdAted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // JavaScript  
 	   
 // DOM variable decleared
-	 
+var darkFontSocial = document.getElementById("dark-font-social");	 
 var logoNav = document.getElementById("logo-nav");
 var logo = document.getElementById("logo");
 var sitename = document.getElementsByClassName("sitename");
@@ -21,19 +21,26 @@ var asideRightAnchor = document.querySelectorAll("#aside-right a");
 var learningDiv = document.querySelectorAll("#learning > div");
 var active = document.getElementsByClassName("active");    
 var rightAnchorLi = document.querySelectorAll("#aside-right a li");
-var w3DropList = document.querySelectorAll("#logo-nav li ul");
-var navLi = document.querySelectorAll("#logo-nav li");
-var navUl = document.querySelectorAll("#logo-nav #nav > ul");
+var w3schoolDrop = document.getElementById("w3school-com");
+var w3DropList = document.getElementsByClassName("dropdown-list");
+var navA = document.querySelectorAll("#logo-nav #nav > a");
+// var navUl = document.querySelectorAll("#logo-nav #nav > ul");
 var navLogonav = document.querySelectorAll("#logo-nav #nav");
 var menuBar = document.getElementById("menu");
+var dfsMenu = document.getElementById("menu-dfs");
 var rightMenuOpen = document.getElementById("right-menu-open");
+var leftMenuOpen = document.getElementById("left-menu-open");
 var rightMenuClose = document.getElementById("right-menu-close");
+var leftMenuClose = document.getElementById("left-menu-close");
 var slide = document.getElementsByClassName("slide");
 var next = document.getElementsByClassName("next");
 var prev = document.getElementsByClassName("prev");
+var newSlide = document.getElementsByClassName("a-slide");
+ 
+
 var countDarkTheme = 0;
 var countMenu = 0;
-
+var countDFS = 0;
 	
 // counter  function for darkTheme
   var counterDarkTheme = (function(){ 
@@ -50,6 +57,16 @@ var countMenu = 0;
     return countMenu;
     }
     })(); 
+	
+// counter function for menudfs 
+
+var counterDFS = (function() {
+	return function() {
+	countDFS +=1;
+	return countDFS;
+	}
+    })();    
+
 
 // slide function
     var slideIndex = 1;
@@ -62,30 +79,59 @@ var countMenu = 0;
 	 //nothing
 	}
     }
-
-    // function currentSlide(n) {
-	    // showSlide(slideIndex = n);
-    // }		  
+	  
           
 
       function showSlides(n) {
         var i;
-        if(n > slide.length) {
+
+	if(window.innerWidth < 1050 && window.innerWidth > 650) {
+		if(n > slide.length) {
 	     slideIndex = 1;
         }
    
         if(n < 1 ) {
 	     slideIndex = slide.length 
         }
+		
+		for(i=0; i<newSlide.length; i++) {
+	      newSlide[i].style.display = "inline-block";
+        }
    
          for(i=0; i<slide.length; i++) {
 	      slide[i].style.display = "none";
         }
+         
+		 slide[slideIndex - 1].style.display = "inline-block";
+		}
+		
+	else if(window.innerWidth < 650) {
+        
+		if(n > newSlide.length) {
+	     slideIndex = 1;
+        }
    
-         slide[slideIndex - 1].style.display = "inline-block";
+        if(n < 1 ) {
+	     slideIndex = newSlide.length 
+        }
+		
+		 for(i=0; i<slide.length; i++) {
+	      slide[i].style.display = "inline-block";
+        }
+		
+         for(i=0; i<newSlide.length; i++) {
+	      newSlide[i].style.display = "none";
+        }
+		 newSlide[slideIndex - 1].style.display = "inline-block";
+		}
+		
+		else {
+		 //nothing
+		}
+         
 
         }
-// darkTheme function dark mode control
+//darkTheme function dark mode control
 	  
  var darkTheme = function() {
 	
@@ -318,97 +364,199 @@ var countMenu = 0;
 //menu function 
 	
 	function menuFunction() {
-		var pos = -50;
-	    if(counterMenu() % 2 !== 0) {
+	  var pos = -100;
+
+	  if(counterMenu() % 2 !== 0) {
 		 
-		 var clearItOpen = setInterval(menuAniOpen, 20);
+		var clearItOpen = setInterval(menuAniOpen, 1);
 		 
-		 function menuAniOpen() {
-			asideLeft.style.display = "block";
-			navLogonav[0].style.display = "block";
-			if(pos == 0) {
-				clearInterval(clearItOpen);
-			}
-			else {
-				pos++;
-				asideLeft.style.right = pos + "%";
-			}
+		function menuAniOpen() { 
+		 
+		 if(window.innerWidth < 1050 && window.innerWidth > 650) {
+
+		 if(pos == 0) {
+		  clearInterval(clearItOpen); 
+		  navLogonav[0].style.display = "block";
+		  
+		}
+		  
+		 else {
+		  pos++;
+		  asideLeft.style.display = "block";
+		  asideLeft.style.right = pos + "%";
+		  asideLeft.style.left = null;
+
+		}
+		}
+		
+		else if(window.innerWidth < 650) {
+		 navLogonav[0].style.display = "block";
+		 clearInterval(clearItOpen);
+		} 
+		
+		else {
+		 clearInterval(clearItOpen);
+		}
+	}
+		 
+	}
+		
+	  else {
+		var pos = 0;
+		var clearItClose = setInterval(menuAniClose, 1);
+
+		function menuAniClose() {
+		 if(window.innerWidth < 1050 && window.innerWidth > 650) {
+			 
+		  if(pos == -50) { 
+		   clearInterval(clearItClose);
+	       navLogonav[0].style.display = "none";
+		   asideLeft.style.display = "none";
 		 }
 		 
+		  else {
+		   pos--;
+		   asideLeft.style.right = pos + "%";
+		}
 		}
 		
+		else if(window.innerWidth < 650) {
+		 navLogonav[0].style.display = "none";
+		 asideLeft.style.display = "none";
+		 clearInterval(clearItClose);
+		 
+		}
+			
 		else {
-		 var pos = 0;
-		 var clearItClose = setInterval(menuAniClose, 10);
-
-		 
-		 
-		 function menuAniClose() {
-			if(pos == -50) {
-				clearInterval(clearItClose);
-				navLogonav[0].style.display = "none";
-				asideLeft.style.display = "none";
-			}
-			else {
-				pos--;
-				asideLeft.style.right = pos + "%";
-			}
+		// clearInterval(clearItClose);
 		}
+	}
 
 		
 	}
 	}
+  // dfsmenu function 
+  
+   function menuDFS() {
+	   if(counterDFS() % 2 !== 0) {
+		  darkFontSocial.style.height = "auto";
+		  menuBar.style.top = "-257px";
+		  var i, len;
+		  len = newSlide.length;
+		  for(i=0; i<len; i++) {
+			newSlide[i].style.display = "block";
+			newSlide[i].style.textAlign = "center";
+			
+		  }
+	   }
+	   
+	   else {
+		  darkFontSocial.style.height = "50px";
+		  menuBar.style.top = "-57px";
+		  var i, len;
+		  len = newSlide.length;
+		  for(i=0; i<len; i++) {
+			newSlide[i].style.display = "inline-block";
+			newSlide[i].style.textAlign = "center";
+			
+		  }
+		  
+		  showSlides(slideIndex);
+	   }
+   }
+
+  // close all menu
 	
 	content.onclick = function() {
-		var pos = 0;
-		var post = 0;
-		var point = 25;
-		if(window.innerWidth < 1050) {
-		var clearIt = setInterval(menuClose, 10);
-		
-		function menuClose() {
-			if(post <= -25 && point <= 0 && pos <= -50) {
-				clearInterval(clearIt);
-				asideRight.style.display = "none";
-				rightMenuOpen.style.right = "0%";
-			    rightMenuOpen.style.display = "inline-block";
-			    rightMenuClose.style.display = "none";
-				asideLeft.style.display = "none";
-				navLogonav[0].style.display = "none";
-                countMenu = 0;
-			}
+	 if(window.innerWidth < 1050 && window.innerWidth> 650) {
+		asideLeft.style.display = "none";
+		asideRight.style.display = "none";
+		navLogonav[0].style.display = "none";
+		leftMenuClose.style.display = "none";
+		leftMenuOpen.style.left = "0";
+		rightMenuClose.style.display = "none";
+		rightMenuOpen.style.display = "inline-block";
+		rightMenuOpen.style.right = "0";
+		countMenu = 0;
+	 }
+	 
+	 else if(window.innerWidth < 650) {
+		 leftMenuOpen.style.display = "inline-block";
+		 asideLeft.style.display = "none";
+		 asideRight.style.display = "none";
+		 navLogonav[0].style.display = "none";
+		 leftMenuClose.style.display = "none";
+		 leftMenuOpen.style.left = "0";
+		 rightMenuClose.style.display = "none";
+		 rightMenuOpen.style.display = "inline-block";
+		 rightMenuOpen.style.right = "0";
+		 countMenu = 0;
+	 }
+	} 
+	
+// left aside menu open function at window.innerHeight < 650
+
+     function asideLeftMenuOpen() {
+		 var pos = -75;
+		 var point = 0;
+		 var post = 0;
+		 var points = 75;
+		 var clearIt = setInterval(menuAni, 1);
+         
+
+	function menuAni() {
+		if(window.innerWidth < 650) {
+		  
+		 if(pos == 0 && point == 75 && post == -75 && points == 0) {
+		  clearInterval(clearIt);
+		  asideRight.style.display = "none";
+		  rightMenuOpen.style.display = "inline-block";
+		  rightMenuOpen.style.right = "0";
+		  rightMenuClose.style.display = "none";
+		  leftMenuOpen.style.display  = "none";
+		  leftMenuClose.style.display = "inline-block";
+		  leftMenuClose.style.left = "75%";
+		 }
+		 
+		 else {
+			pos++;
+			point++;
+			post--;
+			points--;
+			asideRight.style.right = post + "%";
+			rightMenuClose.style.right = points + "%";
+			asideLeft.style.display = "block";
+			asideLeft.style.left = pos + "%";
+			asideLeft.style.right = null;
+			leftMenuOpen.style.left = point + "%";
 			
-			else {
-				pos--;
-				post--;
-				point--;
-				asideLeft.style.right = pos + "%";
-				asideRight.style.right = post + "%";
-				rightMenuClose.style.right = point + "%";
-			}
-		}
-		}
-		
-		else {
-		 //nothing
-		}
+		 }
 	}
+	} 
+	 }
 	
 // right aside menu open function	
 	
 	 function asideRightMenuOpen() {
 		 var pos = -25;
 		 var point = 0;
-		 var clearIt = setInterval(menuAni, 20);
+		 var post = -75;
+		 var points = 0;
+		 var nVePost = 0;
+		 var nVePoints = 75;
+		 var clearIt = setInterval(menuAni, 1);
          
 
-		 function menuAni() {
-         if(pos == 0 && point == 25) {
+	function menuAni() {
+         
+		if(window.innerWidth < 1050 && window. innerWidth > 650) {
+          if(pos == 0 && point == 25) {
 			 clearInterval(clearIt);
 			 rightMenuOpen.style.display  = "none";
 			 rightMenuClose.style.display = "inline-block";
 			 rightMenuClose.style.right = "25%";
 		 }
+		
 		 else {
 			pos++;
 			point++;
@@ -416,19 +564,77 @@ var countMenu = 0;
 			asideRight.style.right = pos + "%";
 			rightMenuOpen.style.right = point + "%";
 		 }
+		 
 		 }
-	 }
-	 
+		 
+		else if(window.innerWidth < 650) {
+	
+		 if(post == 0 && points == 75) {
+		  clearInterval(clearIt);
+		  asideLeft.style.display = "none";
+		  leftMenuClose.style.display = "none";
+		  leftMenuOpen.style.display = "inline-block";
+		  leftMenuOpen.style.left = "0";
+		  rightMenuOpen.style.display  = "none";
+		  rightMenuClose.style.display = "inline-block";
+		  rightMenuClose.style.right = "75%";
+		 }
+		 
+		 else {
+			post++;
+			points++;
+			nVePoints--;
+			nVePost--;
+			asideLeft.style.left =  nVePost + "%";
+			leftMenuClose.style.left = nVePoints + "%";
+			asideRight.style.display = "block";
+			asideRight.style.right = post + "%";
+			rightMenuOpen.style.right = points + "%";
+			
+		 }
+	}
+	}
+	}
+	
+// left aside menu close function only at window.innerHeight < 650
+	     function asideLeftMenuClose() {
+		   var pos = 0;
+		   var point = 75;	
+
+		 var clearIt = setInterval(menuAni, 1);
+         
+
+		 function menuAni() {
+		  if(window.innerWidth < 650) {
+			if(pos == -75 && point == 0) {
+			 clearInterval(clearIt);
+			 leftMenuOpen.style.left = "0%";
+			 leftMenuOpen.style.display  = "inline-block";
+			 leftMenuClose.style.display = "none";
+			 asideLeft.style.display = "none";
+		 }
+		 else {
+			pos--;
+			point--;
+			asideLeft.style.left = pos + "%";
+			leftMenuClose.style.left = point + "%";
+		 } 
+		 }  
+		 }
+		 }
 	 
 // right aside menu close function
 
         function asideRightMenuClose() {
 		 var pos = 0;
 		 var point = 25;
-		 var clearIt = setInterval(menuAni, 20);
+		 var post = 0;
+		 var points = 75;
+		 var clearIt = setInterval(menuAni, 1);
          
 
 		 function menuAni() {
+		 if(window.innerWidth < 1050 && window.innerWidth > 650) {
          if(pos == -25 && point == 0) {
 			 clearInterval(clearIt);
 			 rightMenuOpen.style.right = "0%";
@@ -442,6 +648,24 @@ var countMenu = 0;
 			asideRight.style.right = pos + "%";
 			rightMenuClose.style.right = point + "%";
 		 }
+		 }
+		 
+		 else if(window.innerWidth < 650) {
+			if(post == -75 && points == 0) {
+			 clearInterval(clearIt);
+			 rightMenuOpen.style.right = "0%";
+			 rightMenuOpen.style.display  = "inline-block";
+			 rightMenuClose.style.display = "none";
+			 asideRight.style.display = "none";
+		 }
+		 else {
+			post--;
+			points--;
+			asideRight.style.right = post + "%";
+			rightMenuClose.style.right = points + "%";
+		 } 
+		 }
+		 
 		 }
 	 }
 	
@@ -528,27 +752,29 @@ var countMenu = 0;
 	  logoNav.style.position = "fixed";
 	  logoNav.style.top = "0";
 	  logoNav.style.width = "100%";
-	  logo.style.width = "55px";
-	  logo.style.height = "55px";
+	  logo.style.width = "50px";
+	  logo.style.height = "50px";
 	  navLogonav[0].style.height = "60px";
 	  navLogonav[0].style.top = "60px";
 	  content.style.marginTop = "75px"; 
-	//  w3DropList[0].style.top = "100%";
-	  navUl[0].style.height = "50px";
+	  //w3DropList[0].style.top = "100%";
+	  w3schoolDrop.style.lineHeight = "58px";
 	  asideLeft.style.height = window.innerHeight - 70 + "px";
 	  asideRight.style.height = window.innerHeight - 70 + "px";
 	  menuBar.style.lineHeight = "50px";
+	  
+	  
 	  
 	  
     var i; 	  
 	var len = sitename.length;
 	var lenNav = navItem.length;
 	var lenDot = hoverDot.length;
-	var lenNavLi = navLi.length;
+	var lennavA = navA.length;
 	
 	for(i=0; i<len; i++) {
 	  sitename[i].style.fontSize = "35px";
-	  sitename[i].style.lineHeight = "55px";
+	  sitename[i].style.lineHeight = "57px";
     }
 	
 	for(i=0; i<lenNav; i++) {
@@ -559,15 +785,37 @@ var countMenu = 0;
 		hoverDot[i].style.top = "80%";
 	}
 	
-	  for(i=0; i<lenNavLi; i++) {
-		  navLi[i].style.height = "50px";
+	  for(i=0; i<lennavA; i++) {
+		  navA[i].style.lineHeight = "58px";
 	}
 	  
-	  if(window.innerWidth < 1050) {
-       asideLeft.style.height = window.innerHeight - 137 + "px";
-	   asideRight.style.height = window.innerHeight - 77 + "px";
+	  if(window.innerWidth < 1050 && window.innerWidth > 650) {
+       asideLeft.style.height = window.innerHeight - 120 + "px";
+	   asideRight.style.height = window.innerHeight - 60 + "px";
 	   asideLeft.style.top = "115px";
 	   asideRight.style.top = "55px";
+	}
+	
+	else if (window.innerWidth < 650) {
+		asideLeft.style.top = "55px";
+		asideRight.style.top = "55px";
+		asideRight.style.height = window.innerHeight - 60 + "px";
+	    asideRight.style.height = window.innerHeight - 60 + "px";
+		menuBar.style.top = "15px";
+		navLogonav[0].style.height = "auto";
+	  if(countDFS % 2 !== 0) {
+		  darkFontSocial.style.height = "50px";
+		  var i, len;
+          len = newSlide.length;
+          
+          for(i=0; i<len; i++) {
+			  newSlide[i].display = "inline-block";
+		  }		  
+		   
+		  showSlides(slideIndex);
+		  countDFS = 0;
+	  }
+		
 	}
 	  
 	  else {
@@ -591,23 +839,23 @@ var countMenu = 0;
 	  logoNav.style.width = "auto";
 	  logo.style.width = "70px";
 	  logo.style.height = "70px";
-	  navLogonav[0].style.height = "70px";
+	  navLogonav[0].style.height = "80px";
 	  navLogonav[0].style.top = "80px";
 	  content.style.marginTop = "5px";
-	  navUl[0].style.height = "70px";
+	  w3schoolDrop.style.lineHeight = "78px";
 	  asideLeft.style.height = window.innerHeight -(145 - y) + "px";
 	  asideRight.style.height = window.innerHeight -(145 - y) + "px";
 	  menuBar.style.lineHeight = "70px";
 	  
 	var i;
 	var len = sitename.length;
-	var lenNavLi = navLi.length;
+	var lennavA = navA.length;
 	var lenNav = navItem.length;
 	var lenDot = hoverDot.length;
 	
 	for(i=0; i<len; i++) {
-	  sitename[i].style.fontSize = "45px";
-	  sitename[i].style.lineHeight = "70px";
+	  sitename[i].style.fontSize = "40px";
+	  sitename[i].style.lineHeight = "80px";
  	}
 	
 	
@@ -622,16 +870,25 @@ var countMenu = 0;
 		hoverDot[i].style.top = "70%";
 	}
 	
-	for(i=0; i<lenNavLi; i++) {
-		  navLi[i].style.height = "70px";
+	for(i=0; i<lennavA; i++) {
+		  navA[i].style.lineHeight = "78px";
 	  }
 	  
-	  if(window.innerWidth < 1050) {
-		asideLeft.style.height = window.innerHeight - (222 - y) + "px";
-	    asideRight.style.height = window.innerHeight - (152 - y) + "px";
+	  if(window.innerWidth < 1050 && window.innerWidth > 650) {
+		asideLeft.style.height = window.innerHeight - (205 - y) + "px";
+	    asideRight.style.height = window.innerHeight - (135 - y) + "px";
 	    asideLeft.style.top = 200 - y + "px";
 	    asideRight.style.top = 130 - y + "px";
 	}
+	
+	  else if(window.innerWidth < 650) {
+		asideLeft.style.height = window.innerHeight - (135 - y) + "px";
+	    asideRight.style.height = window.innerHeight - (135 - y) + "px";
+	    asideLeft.style.top = 130 - y + "px";
+	    asideRight.style.top = 130 - y + "px"; 
+		menuBar.style.top = "-57px";
+		navLogonav[0].style.height = "auto";
+	  }
 	
 	  else {
 		asideLeft.style.top = "70px";  
@@ -648,29 +905,83 @@ var countMenu = 0;
 // resize event control
 	
 	function resizeEvent() {
-		if(window.innerWidth < 1050) {
-			asideLeft.style.display = "none";
-			asideRight.style.display = "none";
-			rightMenuOpen.style.display = "inline-block";
-			rightMenuOpen.style.right = "0%";
-			rightMenuClose.style.display = "none";
-			
-			 showSlides(slideIndex);
+	  if(window.innerWidth < 1050 && window.innerWidth > 650) {
+		navLogonav[0].style.display = "none";
+		asideLeft.style.display = "none";
+		asideRight.style.display = "none";
+		rightMenuOpen.style.display = "inline-block";
+		rightMenuOpen.style.right = "0%";
+		rightMenuClose.style.display = "none";
+		leftMenuClose.style.display = "none";
+		leftMenuOpen.style.display = "none";
+		navLogonav[0].style.height = "70px";
+		menuBar.style.top = "15px";
+		
+	    showSlides(slideIndex); 
+		
+        countMenu = 0;
+		
+	 if(countDarkTheme % 2 !== 0) {
+		asideLeft.style.backgroundColor = "#282c34";
+	    asideRight.style.backgroundColor = "#282c34"; 
+	}
+	
+	 else {
+		asideLeft.style.backgroundColor = "#fff";
+	    asideRight.style.backgroundColor = "#fff";  
+	 }
 	   
 
 	
 		}
+		
+	 else if(window.innerWidth < 650) {
+		asideLeft.style.display = "none";
+		asideRight.style.display = "none";
+		navLogonav[0].style.display = "none";
+		navLogonav[0].style.height = "auto";
+		rightMenuOpen.style.display = "inline-block";
+		rightMenuOpen.style.right = "0%";
+		rightMenuClose.style.display = "none";
+		
+		
+		leftMenuOpen.style.display = "inline-block";
+		leftMenuOpen.style.left = "0%";
+		leftMenuClose.style.display = "none";
+		navLogonav[0].style.height  = "auto";
+		countMenu = 0;	
+	    showSlides(slideIndex); 
+		
+		if(document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+			menuBar.style.top = "15px";
+		} 
+		
+		else {
+			menuBar.style.top = "-57px";
+		}
+		
+		}	
+		
+		//else part
 		
 		else {
 			asideLeft.style.display = "block";
 			asideRight.style.display = "block";
 			rightMenuOpen.style.display = "none";
 			rightMenuClose.style.display = "none";
-		    var i, len;
+			leftMenuClose.style.display = "none";
+		    leftMenuOpen.style.display = "none";
+			navLogonav[0].style.display = "block";
+		    var i, len, newLen;
 			len = slide.length;
+			newLen = newSlide.length;
 			for(i=0; i<len; i++) {
 				slide[i].style.display = "inline-block"; 
 			} 
+			for(i=0; i<newLen; i++) {
+				newSlide[i].style.display = "inline-block"; 
+			}
+			
 		}
 	}
 
